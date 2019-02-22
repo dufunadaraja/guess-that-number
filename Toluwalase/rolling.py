88f379c5efbcf5f6e2610ea79193c7477d30577b
Tolu Akin <toluakintoye@gmail.com>
2019-02-22 10:55:49 +0100

    Double six is working

--- Toluwalase/rolling.py
+++ Toluwalase/rolling.py
@@ -1,14 +1,17 @@
 import random
 
 def dice_roll():
-    print("Your number is: " + str(random.randint(1,6)))
+    random_no = random.randint(1,6)
+    print("Your number is: " + str(random_no))
+    while random_no == 6:
+        random_no = random.randint(1, 6)
+        print("Double six: " + str(random_no))
+        #dice_roll()
     play_again = input("Would you like to play again? ").lower()
 
     if play_again == "yes":
-            dice_roll()
-    elif play_again == 6:
-            print("Your number is: " + str(random.randint(1,6)))
+        dice_roll()
     else:
-            print("Game over!")
+        print("Game over!")
     
 dice_roll()
