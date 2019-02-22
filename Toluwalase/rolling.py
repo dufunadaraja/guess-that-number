f6f42ed78d5a0fd4c03b8d6953831b17de43b4b1
Tolu Akin <toluakintoye@gmail.com>
2019-02-22 10:57:32 +0100

    comment removed

--- Toluwalase/rolling.py
+++ Toluwalase/rolling.py
@@ -6,7 +6,6 @@ def dice_roll():
     while random_no == 6:
         random_no = random.randint(1, 6)
         print("Double six: " + str(random_no))
-        #dice_roll()
     play_again = input("Would you like to play again? ").lower()
 
     if play_again == "yes":
